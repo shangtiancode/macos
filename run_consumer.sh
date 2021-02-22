e25ab3d4f054be7ae7733aa2a613a20ae8388cc0
墒田 <xiaobing.mxb@alibaba-inc.com>
2021-02-22 16:16:58 +0800

    修改启动参数

--- run_consumer.sh
+++ run_consumer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -Dspring.config.location=application.consumer.yml -Xmx1g -Xms500m -XX:+UseG1GC -verbose:gc -Xloggc:/usr/local/log/macos.consumer.gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintPromotionFailure -XX:+PrintGCApplicationStoppedTime -XX:+PrintHeapAtGC -jar mubbo-consumer-0.0.1.release.jar >/dev/null 2>&1 &
+nohup java -Dspring.config.location=application.consumer.yml -Xmx500m -XX:+UseG1GC -verbose:gc -Xloggc:/usr/local/log/macos.consumer.gc.log -XX:ErrorFile=/usr/local/log/macos.consumer.error_%p.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintPromotionFailure -XX:+PrintGCApplicationStoppedTime -XX:+PrintHeapAtGC -jar mubbo-consumer-0.0.1.release.jar >/dev/null 2>&1 &
